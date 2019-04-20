ab917152c04b21b97d196d1cd6b9739344735fbe
Abram Saleh <abram9513@gmail.com>
2019-04-20 20:41:10 +0200

    Create types.h
    Standard Types Definitions

---
+++ LIB/types.h
@@ -0,0 +1,30 @@
+/*
+ * types.h
+ *
+ *  Created on: Apr 15, 2019
+ *      Author: Abram Saleh
+ */
+
+#ifndef LIB_TYPES_H_
+#define LIB_TYPES_H_
+
+/**********************************************************************************/
+/************************* Standard Types Definitions *****************************/
+/**********************************************************************************/
+
+typedef unsigned char u8;
+typedef signed char s8;
+
+typedef unsigned short int u16;
+typedef signed short int s16;
+
+typedef unsigned long int u32;
+typedef signed long int s32;
+
+typedef unsigned long long int u64;
+typedef signed long long int s64;
+
+typedef float f32;
+typedef double f64;
+
+#endif /* LIB_TYPES_H_ */
